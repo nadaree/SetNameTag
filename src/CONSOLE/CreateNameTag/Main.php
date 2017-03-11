01dc06d446c24b96c881765b924631d7feb33f3f
DarkArt <huee303@gmail.com>
2017-03-12 00:25:10 +0900

    version 2.1 and English support(To some extent)

--- src/CONSOLE/CreateNameTag/Main.php
+++ src/CONSOLE/CreateNameTag/Main.php
@@ -6,6 +6,9 @@ use onebone\economyapi\EconomyAPI;
 use pocketmine\utils\Config;
 
 class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
+/////////////////
+/*>>[Enabled]<<*/
+/////////////////
 public function onEnable(){
 $t = $this;
 $t->getServer()->getPluginManager()->registerEvents($t, $t);
@@ -17,17 +20,27 @@ $t->config = new Config($t->getDataFolder() . "config.yml", Config::YAML, array(
 if($t->getServer()->getPluginManager()->getPlugin("EconomyAPI") != null){
 $t->EconomyAPI = $t->getServer()->getPluginManager()->getPlugin("EconomyAPI");
 $t->getLogger()->info("§bEconomyAPIの検出を完了");
+//$t->getLogger()->info("§bEconomyAPI is found!");
 }else{
 $t->getLogger()->critical("EconomyAPIが見つかりません。終了します");
+//$t->getLogger()->critical("EconomyAPI is not found. plugin disable:(");
 $t->getServer()->getPluginManager()->disablePlugin($this);}
 }
 public function onCommand(CommandSender $use, Command $cmd, $label, array $args){
 $cost = $this->config->get("金額");$name = $use->getName();
 $money = EconomyAPI::getInstance()->myMoney($use->getName());
-//メッセージ
+//////////////////
+/*>>[Messages]<<*/
+//////////////////
 $m1 = "*/title <称号名>";
 $m2 = "称号を作るには$".$cost."を使います";
 $m3 = "お金が足りません。";
+//English:
+/*
+$m1 = "§a>§etitle <titlename>";
+$m2 = "§b>§eCreateTitle usemoney is $".$cost;
+$m3 = "Your Your short of money.";
+*/
 
 if($use instanceof Player){
 switch($cmd->getName()){
@@ -36,7 +49,6 @@ switch($cmd->getName()){
 	if (empty($args[0])){
 		$use->sendMessage($m1);
 		$use->sendMessage($m2);
-	    //$use->sendMessage("注意: 一度買ったらお金は戻ってきません。");
 		return;
 	}
 
@@ -48,7 +60,8 @@ switch($cmd->getName()){
 			$use->sendMessage($m3); break;
 			}
 		if($money > $cost){
-				$use->sendMessage("購入しました! 称号名:[".$title."§r]");
+			$use->sendMessage("購入しました! 称号名:[".$title."§r]");
+				//$use->sendMessage("Purchase! titlename:[".$title."§r]");
 				EconomyAPI::getInstance()->reduceMoney($name, $cost);
 				$this->user->set($name, $title);
 				$this->user->save();
@@ -58,6 +71,9 @@ switch($cmd->getName()){
 return false;
 }
 
+//////////////////
+///*>>[Chat]<<*///
+//////////////////
 public function Cht(\pocketmine\event\player\PlayerChatEvent $ec)
 {
 $player = $ec->getPlayer();
